f6f0415fe7eb279b9d10e060a066cb9d6cd9fda8
Sourcery AI <>
2022-03-28 02:28:50 +0000

    'Refactored by Sourcery'

--- python/prophet/models.py
+++ python/prophet/models.py
@@ -18,9 +18,7 @@ import platform
 import logging
 logger = logging.getLogger('prophet.models')
 
-PLATFORM = "unix"
-if platform.platform().startswith("Win"):
-    PLATFORM = "win"
+PLATFORM = "win" if platform.platform().startswith("Win") else "unix"
 
 class IStanBackend(ABC):
     def __init__(self):
@@ -108,16 +106,14 @@ class CmdStanPyBackend(IStanBackend):
         try:
             self.stan_fit = self.model.optimize(**args)
         except RuntimeError as e:
-            # Fall back on Newton
-            if self.newton_fallback and args['algorithm'] != 'Newton':
-                logger.warning(
-                    'Optimization terminated abnormally. Falling back to Newton.'
-                )
-                args['algorithm'] = 'Newton'
-                self.stan_fit = self.model.optimize(**args)
-            else:
+            if not self.newton_fallback or args['algorithm'] == 'Newton':
                 raise e
 
+            logger.warning(
+                'Optimization terminated abnormally. Falling back to Newton.'
+            )
+            args['algorithm'] = 'Newton'
+            self.stan_fit = self.model.optimize(**args)
         params = self.stan_to_dict_numpy(
             self.stan_fit.column_names, self.stan_fit.optimized_params_np)
         for par in params:
@@ -199,11 +195,7 @@ class CmdStanPyBackend(IStanBackend):
         end = 0
         two_dims = len(data.shape) > 1
         for cname in column_names:
-            if "." in cname:
-                parsed = cname.split(".")
-            else:
-                parsed = cname.split("[")
-
+            parsed = cname.split(".") if "." in cname else cname.split("[")
             curr = parsed[0]
             if prev is None:
                 prev = curr
@@ -219,10 +211,7 @@ class CmdStanPyBackend(IStanBackend):
                     output[prev] = np.array(data[start:end])
                 prev = curr
                 start = end
-                end += 1
-            else:
-                end += 1
-
+            end += 1
         if prev in output:
             raise RuntimeError(
                 "Found repeated column name"
@@ -269,22 +258,18 @@ class PyStanBackend(IStanBackend):
         try:
             self.stan_fit = self.model.optimizing(**args)
         except RuntimeError as e:
-            # Fall back on Newton
-            if self.newton_fallback and args['algorithm'] != 'Newton':
-                logger.warning(
-                    'Optimization terminated abnormally. Falling back to Newton.'
-                )
-                args['algorithm'] = 'Newton'
-                self.stan_fit = self.model.optimizing(**args)
-            else:
+            if not self.newton_fallback or args['algorithm'] == 'Newton':
                 raise e
 
-        params = {}
-
-        for par in self.stan_fit.keys():
-            params[par] = self.stan_fit[par].reshape((1, -1))
-
-        return params
+            logger.warning(
+                'Optimization terminated abnormally. Falling back to Newton.'
+            )
+            args['algorithm'] = 'Newton'
+            self.stan_fit = self.model.optimizing(**args)
+        return {
+            par: self.stan_fit[par].reshape((1, -1))
+            for par in self.stan_fit.keys()
+        }
 
     def load_model(self):
         """Load compiled Stan model"""
@@ -305,4 +290,4 @@ class StanBackendEnum(Enum):
         try:
             return StanBackendEnum[name].value
         except KeyError as e:
-            raise ValueError("Unknown stan backend: {}".format(name)) from e
+            raise ValueError(f"Unknown stan backend: {name}") from e
